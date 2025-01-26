77ec30986f673b1c986e64c7d924bb5c73d47bc0
Александр Елисеев <a.eliseev@demiusgroup.ru>
2025-01-26 22:44:25 +0300

    changed limit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filter/http.exception.filter';
 import * as cookieParser from 'cookie-parser';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(process.env.PORT ?? 3010);
 }
 bootstrap();
